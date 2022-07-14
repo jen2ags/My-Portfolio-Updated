f9b851872c2b9d0dfd230ad1d9815c652854e993
jennj <jennjenn93@gmail.com>
2022-07-14 11:46:29 -0500

    Updates

---
+++ precache-manifest.265bbe6b52432a9944ec30b509dfe48a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b99d0d2fead9384ae197",
+    "url": "/My-React-Portfolio/static/css/main.95941ab2.chunk.css"
+  },
+  {
+    "revision": "b99d0d2fead9384ae197",
+    "url": "/My-React-Portfolio/static/js/main.b99d0d2f.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/My-React-Portfolio/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "4366da911fe84e4c73bd",
+    "url": "/My-React-Portfolio/static/js/2.4366da91.chunk.js"
+  },
+  {
+    "revision": "96db2ab1a608d6344660",
+    "url": "/My-React-Portfolio/static/js/runtime~main.96db2ab1.js"
+  },
+  {
+    "revision": "0e868efa422fdb79c6ee852d0b37454e",
+    "url": "/My-React-Portfolio/static/media/profile.0e868efa.jpg"
+  },
+  {
+    "revision": "daff09d904af52a111c5087fe6faf8f4",
+    "url": "/My-React-Portfolio/static/media/Jennifer_Jennings_Resume.daff09d9.pdf"
+  },
+  {
+    "revision": "2223fad01c6a158542a682e086560f9e",
+    "url": "/My-React-Portfolio/static/media/medportal.2223fad0.png"
+  },
+  {
+    "revision": "e7886882633c2692f10181f32f99d5c6",
+    "url": "/My-React-Portfolio/static/media/karaoke-kings.e7886882.png"
+  },
+  {
+    "revision": "1f2ead019123381073589f87bcc32f2f",
+    "url": "/My-React-Portfolio/static/media/note-taker.1f2ead01.png"
+  },
+  {
+    "revision": "57ae9cdc94da44097fbaf91a8d9d3bcc",
+    "url": "/My-React-Portfolio/static/media/robot-gladiators.57ae9cdc.png"
+  },
+  {
+    "revision": "7f67f16b525d587285083cbbf786680e",
+    "url": "/My-React-Portfolio/static/media/run-buddy.7f67f16b.png"
+  },
+  {
+    "revision": "0fa35eb2e053db9ac141a1b9ea502271",
+    "url": "/My-React-Portfolio/static/media/work-day-scheduler.0fa35eb2.png"
+  },
+  {
+    "revision": "f43d1bdc0b260fdaec9f826e767cd5ae",
+    "url": "/My-React-Portfolio/static/media/motor-bike-labs.f43d1bdc.png"
+  },
+  {
+    "revision": "265349feea2ad707077fd3a2615aa0b0",
+    "url": "/My-React-Portfolio/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/My-React-Portfolio/precache-manifest.265bbe6b52432a9944ec30b509dfe48a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/My-React-Portfolio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
